4b9e8659a53cdf09326ac38e3b73518f2f9d56a3
maxnyquist <max.nyquist@mass.gov>
2021-12-23 11:11:17 -0500

    Update ProfileImport.R to include secchi data import. Create GET_SECCHI function.

--- src/Wachusett/ImportProfiles.R
+++ src/Wachusett/ImportProfiles.R
@@ -1,30 +1,30 @@
 ###############################  HEADER  ######################################
 #  TITLE: ImportProfiles.R
-#  DESCRIPTION: This script will process/import reservoir Profile Data to database
+#  DESCRIPTION: This script will process/import reservoir Profile Data and Secchi Data to database
 #  AUTHOR(S): Dan Crocker, Max Nyquist
-#  DATE LAST UPDATED: 2021-03-31
+#  DATE LAST UPDATED: 2021-12-23
 #  GIT REPO: 
-#  R version 3.6.0 (2019-04-26)  i386
+#  R version 4.0.3 (2020-10-10)
 ##############################################################################.
 
 # COMMENT OUT BELOW WHEN RUNNING FUNCTION IN SHINY
-
-library(tidyverse)
-library(stringr)
-library(odbc)
-library(RODBC)
-library(DBI)
-library(lubridate)
-library(magrittr)
-library(readxl)
-library(DescTools)
-
-# COMMENT OUT ABOVE CODE WHEN RUNNING IN SHINY!
-### MN - bring in data streams for testing.
-datset <- config[8]
-files <- readxl::read_xlsx(datset)
-rawdatafolder <- paste0(files[2,11])
-file <-  paste0("2021_", files[2,14])
+# 
+# library(tidyverse)
+# library(stringr)
+# library(odbc)
+# library(DBI)
+# library(lubridate)
+# library(magrittr)
+# library(readxl)
+# library(DescTools)
+# library(glue)
+# 
+# # COMMENT OUT ABOVE CODE WHEN RUNNING IN SHINY!
+# ### MN - bring in data streams for testing.
+# datset <- config[8]
+# files <- readxl::read_xlsx(datset)
+# rawdatafolder <- paste0(files[2,11])
+# file <-  paste0("2021_", files[2,14])
 
 ########################################################################.
 ###                          Process Data                           ####
@@ -49,32 +49,7 @@ sheetName <- excel_sheets(path)[sheetNum]
 df.wq <- read_excel(path, sheet = sheetNum, range = cell_cols("A:R"),  col_names = F, trim_ws = T, na = "nil") %>%
   as.data.frame()   # This is the raw data - data comes in as xlsx file, so read.csv will not work
 
-# Read in the raw data - defaults to the last sheet added
-df.wq <- read_excel(path, sheet = sheetNum, range = cell_cols("A:R"),  col_names = F, trim_ws = T, na = "nil") %>%
-  as.data.frame()   # This is the raw data - data comes in as xlsx file, so read.csv will not work
-#Separate Secchi notes, analyst, depth data before slice
-df.secchi <- df.wq %>%
-  slice(1:4)
-#Select columns
-df.secchi <- df.secchi[c(2:4), c(1:3,6,7)]
-#Get rid of NA from spreadsheet. Names will be set later
-df.secchi$...3 <- str_replace_na(df.secchi$...3, replacement =  "")
-#Concat to track depth feet and depth meters
-df.secchi <- df.secchi %>%
-  mutate(...1 = str_c(...1, ...3, sep = "_"))
-#df.secchi$...1 <- paste(df.secchi$...1, df.secchi$...3, sep = "_")
-#df.secchi.t <- df.secchi %>%
-# gather(., param, result, -...1)
-#Workaround for binding specific columns below 2 first rows
-df.secchi <- rbind(df.secchi[1:2], setNames(df.secchi[4:5], names(df.secchi)[1:2])) %>%
-  rename(., param = ...1, result = ...2)
-#  Pivot and spread columns.
-df.secchi  <- df.secchi %>%
-  pivot_wider(names_from = param, values_from = result ) %>%
-  select(1:5)
-
-
-
+### Profile dataframe ####
 # Remove unwanted columns, discard first 4 rows, filter out empty rows (NA inf column 1), add a new row
 df.wq <- df.wq %>%
   slice(5:n())
@@ -99,17 +74,10 @@ df.wq <- gather(df.wq, Parameter, Result, c(7:16))
 # DateTimeET
 df.wq$DateTimeET <- as.POSIXct(paste(as.Date(df.wq$Date, format ="%Y-%m-%d"), df.wq$Time, sep = " "), format = "%Y-%m-%d %H:%M", tz = "America/New_York", usetz = T)
 
-#Select columns for Secchi database table
-df.secchi.join <- df.wq[, c(1, 3, 4, 8)]
-#Unique Secchi, should filter to 1 row
-df.secchi.join <- tail(df.secchi.join, n=1)
-#Join df.secchi and df.secchi.join
-df.secchi <- left_join(df.secchi, df.secchi.join, by = c("Secchi_m" = "Secchi Depth"))
-#Remove Secchi join
-rm(df.secchi.join)
 #Get rid of UserID, Date,Time and Secchi
 df.wq <- df.wq[, c(4,12, 10, 7, 11, 5)]
 
+#Round numeric values
 df.wq$Result <- round(as.numeric(df.wq$Result), 3)
 df.wq$DEP <- round(as.numeric(df.wq$DEP),3)
 
@@ -165,8 +133,6 @@ df.wq$DataSourceID <- seq(1, nrow(df.wq), 1)
 ### Importdate (Date)
 df.wq$ImportDate <- today()
 
-
-# Read Tables
 # WQ
 setIDs <- function(){
   query.wq <- dbGetQuery(con, glue("SELECT max(ID) FROM [{schema}].[{ImportTable}]"))
@@ -208,7 +174,7 @@ df.wq$DataSourceID <- as.integer(df.wq$DataSourceID)
 dfs <- list()
 dfs[[1]] <- df.wq
 dfs[[2]] <- path
-dfs[[3]] <- NULL # Removed condition to test for flags and put it in the setFlagIDS() function
+#dfs[[3]] <- NULL # Removed condition to test for flags and put it in the setFlagIDS() function
 
 # Disconnect from db and remove connection obj
 dbDisconnect(con)
@@ -226,19 +192,75 @@ return(dfs)
 ###                       Write Data to Database                    ####
 ########################################################################.
 GET_SECCHI <- function(path,file){
-
-   #return NULL if no data frame 
-  if(is.na(df.secchi$Secchi_ft)){
-   df.secchi <- NULL
-    return(NULL)
-    }else{
-    as.data.frame(df.secchi)
-  }
   
+  df.wq <- read_excel(path, sheet = sheetNum, range = cell_cols("A:R"),  col_names = F, trim_ws = T, na = "nil") %>%
+    as.data.frame()   # This is the raw data - data comes in as xlsx file, so read.csv will not work
+  
+  ### Secchi dataframe part 1 ####
+  #Separate Secchi notes, analyst, depth data before slice
+  df.secchi <- df.wq %>%
+    slice(1:4)
+  #Select columns
+  df.secchi <- df.secchi[c(2:4), c(1:3,6,7)]
+  
+  #Get rid of NA from spreadsheet. Names will be set later
+  df.secchi$...3 <- str_replace_na(df.secchi$...3, replacement =  "")
+  #Concat to track depth feet and depth meters
+  df.secchi <- df.secchi %>%
+    mutate(...1 = str_c(...1, ...3, sep = "_"))
+  if(is.na(df.secchi[2, 2])){
+    df.secchi <- NULL
+  }else{
+    #Workaround for binding specific columns below 2 first rows
+    df.secchi <- rbind(df.secchi[1:2], setNames(df.secchi[4:5], names(df.secchi)[1:2])) %>%
+      rename(., param = ...1, result = ...2)
+    #  Pivot and spread columns.
+    df.secchi  <- df.secchi %>%
+      pivot_wider(names_from = param, values_from = result ) %>%
+      select(1:5)
+    
+    ### Profile dataframe ####
+    # Remove unwanted columns, discard first 4 rows, filter out empty rows (NA inf column 1), add a new row
+    df.wq <- df.wq %>%
+      slice(5:n())
+    # Rename Columns using first row values and then remove the first row
+    names(df.wq) <- unlist(df.wq[1,])
+    df.wq <- df.wq[-1,]
+    
+    df.wq <- df.wq[complete.cases(df.wq[, 1:3]),]
+    
+    # Data class/formats
+    df.wq$Date <- as.numeric(as.character(df.wq$Date))
+    df.wq$Date <- XLDateToPOSIXct(df.wq$Date)
+    df.wq$Time <- as.numeric(as.character(df.wq$Time))
+    df.wq$Time <- XLDateToPOSIXct(df.wq$Time)
+    
+    #Get rid of Time formatting
+    df.wq <- separate(df.wq, Time, into = c("Date2", "Time"), sep = " ")
+    
+    # reformat the Wachusett Profile data to "Tidy" data format ("Long" instead of "Wide")
+    df.wq <- gather(df.wq, Parameter, Result, c(7:16))
+    
+    # DateTimeET
+    df.wq$DateTimeET <- as.POSIXct(paste(as.Date(df.wq$Date, format ="%Y-%m-%d"), df.wq$Time, sep = " "), format = "%Y-%m-%d %H:%M", tz = "America/New_York", usetz = T)
+    
+    ### Secchi dataframe completion ####
+    #Select columns for Secchi database table
+    df.secchi.join <- df.wq[, c(1, 3, 4, 8)]
+    #Unique Secchi, should filter to 1 row
+    df.secchi.join <- tail(df.secchi.join, n=1)
+    #Join df.secchi and df.secchi.join
+    df.secchi <- left_join(df.secchi, df.secchi.join, by = c("Secchi_m" = "Secchi Depth"))
+    df.secchi$Date <- as_date(df.secchi$Date)
+    df.secchi$Secchi_ft <- as.numeric(df.secchi$Secchi_ft)
+    df.secchi$Secchi_m <- as.numeric(df.secchi$Secchi_m) %>% round(digits =  5)
+    df.secchi <- df.secchi %>% 
+      rename("Analyst" = "Analyst_", "Depth_ft" = "Secchi_ft", "Depth_m" = "Secchi_m", "SampleTimeUTC" = "Time", "Station" = "Site") %>% 
+      mutate("Flagged" = NA_character_)
+    
+    return(df.secchi)
+  }
 }
-
-
-GET_SECCHI(path, file)
 
 
 IMPORT_DATA <- function(df.wq, df.flags = NULL, path, file, filename.db, processedfolder, ImportTable, ImportFlagTable = NULL){
@@ -252,24 +274,21 @@ IMPORT_DATA <- function(df.wq, df.flags = NULL, path, file, filename.db, process
   
   con <- dbConnect(odbc::odbc(), dsn = dsn, uid = dsn, pwd = config[35], timezone = tz)  # Get Import Table Columns
   ColumnsOfTable <- dbListFields(con, schema = schema, ImportTable)
-
-  # # Set variable types
-  # varTypes  <- as.character(ColumnsOfTable$TYPE_NAME)
-  # sqlSave(con, df.wq, tablename = ImportTable, append = T,
-  #         rownames = F, colnames = F, addPK = F , fast = F, varTypes = varTypes)
-  #call GET_SECCHI
-  GET_SECCHI(path,file)
- #assign df.secchi to correct value. NULL if no data, clean record if present
-   df.secchi <- GET_SECCHI(path,file)
- #test df.secchi for NULL, if null, let importer know. if data present, confirm
-null.secchi <- function(path,file){  
+  
+  #assign df.secchi to correct value. NULL if no data, clean record if present
+  df.secchi <- GET_SECCHI(path,file)
+  #test df.secchi for NULL, if null, let importer know. if data present, confirm
+  
   if(is.null(df.secchi)){
-    return("No Secchi data. No records imported.")
-    }else{
-      return("{df.secchi$Date} Secchi value of {df.secchi$Depth_ft} imported")
-      }
-}
-
+    print("No Secchi data. No records imported.")
+  }else{
+    secchi.names <- dbListFields(con, schema_name = schema, name = 'tbl_Secchi')
+    df.secchi <- select(df.secchi, secchi.names[2:10])
+    odbc::dbWriteTable(con, DBI::SQL(glue("{database}.{schema}.tbl_Secchi")), value = df.secchi, append = TRUE)
+    print(glue("{df.secchi$Date} Secchi value of {df.secchi$Depth_ft} imported."))
+    
+  }
+  
   
   odbc::dbWriteTable(con, DBI::SQL(glue("{database}.{schema}.{ImportTable}")), value = df.wq, append = TRUE)
   
